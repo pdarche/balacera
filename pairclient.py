fec25a24c5305e3a05167141db57274eb3d71d23
Peter Darche <pdarche@gmail.com>
2012-11-02 19:07:10 -0400

    prepare code for analysis

--- pairclient.py
+++ pairclient.py
@@ -2,21 +2,27 @@ import zmq
 import random
 import sys
 import time
+import sqlite3
 
 port = "5556"
 context = zmq.Context()
 socket = context.socket(zmq.PAIR)
 socket.connect("tcp://localhost:%s" % port)
 
+#sqlite db connection
+conn = sqlite3.connect('balacera.db', isolation_level=None)
+
 while True:
     msg = socket.recv()
-    msg = msg.split(' ')
-    accel = int(msg[1])
-    if accel > 5:
-    	print "processing!"
-    	socket.send("client message to server1")
-    	time.sleep(1)
-    else:
-    	socket.send("client message to server1")
-    	time.sleep(1)
+    print msg
+    socket.send("client message to server1")
+    time.sleep(1)
+    
+    # msg = msg.split(' ')
+
+    # c = conn.cursor()
+    # try:
+    #     c.execute("SELECT * FROM tweets WHERE creation_time > %d", creation_time)
+    # except sqlite3.Error, msg:
+    #     print msg
 
