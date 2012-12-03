d39043369a0c2d515181512ee0032e038f364c2a
Peter Darche <pdarche@gmail.com>
2012-12-02 20:36:06 -0500

    add csv files

--- assets/data/test.py
+++ assets/data/test.py
@@ -4,15 +4,11 @@ import csv
 import time
 import re
 
-f = open("balacera.csv", "r")
+f = open("balacera-nov.csv", "r")
 csv_file = csv.reader(f, delimiter=',')
 
-# for line in csv_file:
-# 	print line[3], line[11]
-
-csvwriter = csv.writer(open("mty_tweets.csv", "w"))
+csvwriter = csv.writer(open("to-label.csv", "w"))
 csvwriter.writerow([ "time", "text", "event" ])
-# # csvwriter.writerow([ "text", "event" ])
 
 pattern = "monter+ey|mty"
 row = 0
@@ -20,11 +16,12 @@ row = 0
 for line in csv_file:
 	if row != 0:
 		try:
-			match = re.search(pattern, line[3])
-			if match:
-				newrow = [ line[11], line[3] , "" ]
-				# newrow = [ line[3] , "" ]
-				csvwriter.writerow(newrow)
+			# match = re.search(pattern, line[3])
+			# if match:
+			newrow = [ line[11], line[3] , "" ]
+			# newrow = [ line[3] , "" ]
+			# csvwriter.writerow(newrow)
+			print newrow
 		except:
 			pass
 	row += 1
